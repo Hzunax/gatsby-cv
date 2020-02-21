c73cdc05953b4d6cfa5e7b06255e7211870cdfc0
Stijn Van Asschodt <stijn.van.asschodt@ing.com>
2020-02-21 13:11:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+[![Build Status](https://travis-ci.org/santosfrancisco/gatsby-starter-cv.svg?branch=master)](https://travis-ci.org/santosfrancisco/gatsby-starter-cv)
+[![GitHub version](https://badge.fury.io/gh/santosfrancisco%2Fgatsby-starter-cv.svg)](https://badge.fury.io/gh/santosfrancisco%2Fgatsby-starter-cv)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+<p align="center">
+  <a href="https://www.gatsbyjs.org">
+    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
+  </a>
+</p>
+<h1 align="center">
+  Gatsby's CV starter
+</h1>
+
+Create your resume in a few minutes with this totally responsive starter using React. Show off your skills, work experiences and activities in github.
+
+
+## 📷 Preview
+
+![Preview](./preview.png)
+
+## 🚀 Quick start
+
+1.  **Create a Gatsby site.**
+
+    Use the Gatsby CLI to create a new site, specifying the default starter.
+
+    ```sh
+    # create a new Gatsby site using the default starter
+    npx gatsby new my-default-starter https://github.com/santosfrancisco/gatsby-starter-cv
+    ```
+
+1.  **Start developing.**
+
+    Navigate into your new site’s directory and start it up.
+
+    ```sh
+    cd my-default-starter/
+    npm run develop
+    ```
+
+1.  **Open the source code and start editing!**
+
+    Your site is now running at `http://localhost:8000`!
+
+    \_Note: You'll also see a second link: `http://localhost:8000/___graphql`. This is a tool you can use to experiment with querying your data. Learn more about using this tool in the [Gatsby tutorial](https://www.gatsbyjs.org/tutorial/part-five/#introducing-graphiql).\_
+
+    Open the `my-default-starter` directory in your code editor of choice and edit `src/pages/index.js`. Save your changes and the browser will update in real time!
+
+1. **Generate production build**
+
+  That command will generate a production build on _public_ folder
+  ```sh
+    npm run build
+  ```
+
+## Configuration
+
+Update the configuration file with your data. The configuration file is in ```data/siteConfig.js```
+
+:warning: NOTE: Please change googleAnalyticsId to your ID.  See https://analytics.google.com for details.
---
+++ data/siteConfig.js
@@ -0,0 +1,82 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m Stijn!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Stijn Van Asschodt',
+  twitterUsername: 'van_asschodt',
+  githubUsername: 'Hzunax',
+  authorAvatar: '/images/Stijn_avatar.png',
+  authorDescription: `Motivated, hard-working developer with an appetite for learning new things. <br />
+    Graduated in 2017 as a Master in Industrial Engineering - Electronics & ICT at KULeuven. <br />
+    In 2018, I became a certified Scrum Master, which has helped me in better understanding the Agile environment at my current employer. <br />
+    I currently work as a frontend developer, mainly with <strong>Javascript, Polymer & Web Components.</strong>`,
+  skills: [
+    {
+      name: 'JavaScript',
+      level: 70
+    },
+    {
+      name: 'HTML',
+      level: 60
+    },
+    {
+      name: 'CSS',
+      level: 60
+    },
+    {
+      name: 'NodeJs',
+      level: 40
+    },
+    {
+      name: 'React',
+      level: 40
+    },
+    {
+      name: 'Git',
+      level: 50
+    }
+  ],
+  jobs: [
+    {
+      company: "ING",
+      begin: {
+        month: 'Oct',
+        year: '2019'
+      },
+      duration: null,
+      occupation: "Frontend developer",
+      description: "Development, integration, maintenance and delivery of web components and an application that serves 30.000 customers daily."
+
+    }, {
+      company: "Exellys",
+      begin: {
+        month: 'Sep',
+        year: '2017'
+      },
+      duration: '2 yr',
+      occupation: "Frontend developer",
+      description: "Developed several Polymer web components and created a web application used by internal employees"
+    },
+    /* ... */
+  ],
+  social: {
+    twitter: "https://twitter.com/van_asschodt",
+    linkedin: "https://www.linkedin.com/in/stijnvanasschodt/",
+    github: "https://github.com/Hzunax",
+    email: "stijn.van.asschodt@gmail.com"
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/coccora.jpg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Stijn Van Asschodt',
+      url: '/',
+    }
+  ]
+}
